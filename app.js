36573a63c1b9f4e92de94064effc81467bb4f68f
Vlad <obf2007@gmail.com>
2025-01-22 10:49:20 +0300

    hw-3

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const fs = require('fs');
+const express = require('express');
 
-console.log("Привіт з Node.js!");
+const app = express();
 
-fs.readFile('data.txt', 'utf8', (err, data)=>{
-    if(err){
-        console.error("Помилка читання файлу:", err);
-        return;
-    }
-    console.log("Вміст файлу data.txt:", data);
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+})
+
+app.get('/',(req, res)=>{
+    res.send('Привіт з Express.js!');
+})
+
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id
+    res.send(`Hello User: ${id}`)
+})
+
+app.post('/users', (req, res)=>{
+    const {name, email} = req.body;
+    res.send(JSON.stringify({message: `Отримано дані: Ім'я - ${name}, Електронна пошта - ${email}`}));
+})
+
+app.listen(3000, ()=>{
+    console.log('Server started on port 3000');
+});
+
+app.use((req,res)=>{
+    res.status(404).type('text/html').send(`<h1>Page Not Found</h1>`);
 })
