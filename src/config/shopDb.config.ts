2650c72ebd313fd36ede4429ea952f373c767b08
markovo4 <obf2007@gmail.com>
2025-01-31 15:47:45 +0200

    hw-4

--- src/config/shopDb.config.ts
+++ src/config/shopDb.config.ts
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/shopDB', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log('✅ MongoDB connected')
-    } catch(error){
-        console.error("❌ MongoDB connection error:", error);
+    } catch(err){
+        console.error("❌ MongoDB connection error:", err);
         process.exit(1);
     }
 }
